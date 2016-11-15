01627e1cad126f0b74db9ab846347526fe2c26bd
Uwe van Heesch <uwe@vanheesch.net>
2016-11-15 16:11:49 +0100

    New welcome message again

--- src/main/java/ebig/han/nl/HelloController.java
+++ src/main/java/ebig/han/nl/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "The origin of this message is a branch.";
+        return "This is a message is a branch.";
     }
 
 }
