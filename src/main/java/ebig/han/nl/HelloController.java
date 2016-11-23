994155ec8ca5483debd0d9457e0a9c525094fd69
Uwe van Heesch <uwe@vanheesch.net>
2016-11-23 17:14:34 +0100

    Minor changes to message.

--- src/main/java/ebig/han/nl/HelloController.java
+++ src/main/java/ebig/han/nl/HelloController.java
@@ -1,14 +1,21 @@
 package ebig.han.nl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
 
 @RestController
 public class HelloController {
 
+    private Log logger = LogFactory.getLog(HelloController.class);
+
     @RequestMapping("/")
     public String index() {
-        System.out.println("A sligher change");
+        this.logger.info("A slighter change");
         return "A message";
     }
 
